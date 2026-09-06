7008934e4224129247b1e80a66415a857ef5e2ee
matinpouya20-eng <matinpouya20@gmail.com>
2026-09-06 16:19:37 +0330

    Start Java learning

---
+++ 01-variables/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int age = 5;
+        double salary = 50000.50;
+        String name = "pouya";
+    boolean isActive = true;
+System.out.println(age);
+System.out.println(salary);
+    System.out.println(name);
+    System.out.println(isActive);
+
+
+     
+    }
+    
